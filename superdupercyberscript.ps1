ad8291e6eacc011dde2d1d5aee5adfd28b5079a3
BeachMiner <tyler.k.hoang@gmail.com>
2019-09-20 10:59:23 -1000

    Update superdupercyberscript.ps1

--- superdupercyberscript.ps1
+++ superdupercyberscript.ps1
@@ -106,3 +106,10 @@ do {
    else {break}
    } while ($service -eq "Y")
    
+#Programs
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize
+$app = Get-WmiObject -Class Win32_Product | Where-Object { 
+    $_.Name -match "Software Name" 
+}
+
+$app.Uninstall()
